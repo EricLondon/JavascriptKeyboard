9f463ec58e29ecbc37d197fe202aed9a145db383
Eric London <github@ericlondon.com>
2022-01-21 11:38:46 -0500

    wip: more scales

--- src/constants/music.js
+++ src/constants/music.js
@@ -2,22 +2,50 @@ export const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#'
 
 export const SCALES = [
   {
-    name: 'Major',
-    steps: [2, 2, 1, 2, 2, 2, 1]
+    name: "Major",
+    steps: [2, 2, 1, 2, 2, 2, 1],
   },
   {
-    name: 'Minor',
-    steps: [2, 1, 2, 2, 1, 2, 2]
+    name: "Minor",
+    steps: [2, 1, 2, 2, 1, 2, 2],
   },
   {
-    name: 'Major Pentatonic',
-    steps: [2, 2, 3, 2, 3]
+    name: "Major Pentatonic",
+    steps: [2, 2, 3, 2, 3],
   },
   {
-    name: 'Minor Pentatonic',
-    steps: [3, 2, 2, 3, 2]
+    name: "Minor Pentatonic",
+    steps: [3, 2, 2, 3, 2],
+  },
+  {
+    name: "Ionian (i) [Major]",
+    steps: [2, 2, 1, 2, 2, 2, 1],
+  },
+  {
+    name: "Dorian (ii)",
+    steps: [2, 1, 2, 2, 2, 1, 2],
+  },
+  {
+    name: "Phrygian (iii)",
+    steps: [1, 2, 2, 2, 1, 2, 2],
+  },
+  {
+    name: "Lydian (iv)",
+    steps: [2, 2, 2, 1, 2, 2, 1],
+  },
+  {
+    name: "Mixolydian (v)",
+    steps: [2, 2, 1, 2, 2, 1, 2],
+  },
+  {
+    name: "Aeolian (vi) [Natural Minor]",
+    steps: [2, 1, 2, 2, 1, 2, 2],
+  },
+  {
+    name: "Locrian (vii)",
+    steps: [1, 2, 2, 1, 2, 2, 2],
   }
-]
+];
 
 export const COLOR_CLASSES = [
   'primary', // blue
